b148156542b28141441ee6d8854f1e1ddad8bcdc
Matteo Gassend <matteo.gassend@epitech.eu>
2022-02-11 13:47:41 +0100

    [refact] moved authentication logic in separate service and updated readme

--- README.md
+++ README.md
@@ -13,14 +13,18 @@
 ### Initialization
 
 there is a single available export in this library.
+This export contains the client you'll need to use in order to talk with TMDB's api.
 
+#### Using the client in a projet
 ```js
 import Client from "@matfire/the_movie_wrapper"
 
 const client = new Client("<insert_your_api_key>");
 ```
 
-### Available Methods
+### Available Services
+
+Each section of the API has been divided into sections. Taking the code example above, calling the authentication service is done like so: `client.auth.createAuthUrl()`.
 
 #### Authentication
 - getAuthenticationToken() -> retrieve an authentication token for user authentication
--- src/models/API.ts
+++ src/models/API.ts
@@ -1,4 +1,5 @@
 import axios, { AxiosInstance } from 'axios';
+import AuthenticationService from '../services/authentication';
 import TrendingTimeSpan from '../types/generic';
 import Movie from './Movie';
 
@@ -7,36 +8,13 @@ class API {
 
   $http: AxiosInstance;
 
+  auth: AuthenticationService;
+
   constructor(apiKey:string) {
     this.apiKey = apiKey;
     this.$http = axios.create({ baseURL: 'https://api.themoviedb.org/3', params: { api_key: apiKey } });
-  }
 
-  async getTrending(timeSpan: TrendingTimeSpan): Promise<Movie[]> {
-    const res = await this.$http.get(`/trending/all/${timeSpan}`);
-    return res.data.results.map((e: any) => Movie.fromJson(e));
-  }
-
-  async getAuthenticationToken(): Promise<string> {
-    const res = await this.$http.get('/authentication/token/new');
-    if (!res.data.success) {
-      throw new Error('Could not get request token');
-    }
-    return res.data.request_token;
-  }
-
-  async createAuthUrl(redirectUrl: string): Promise<string> {
-    const token = await this.getAuthenticationToken();
-
-    return `https://www.themoviedb.org/authenticate/${token}?redirect_to=${redirectUrl}`;
-  }
-
-  async createSession(requestToken:string): Promise<string> {
-    const res = await this.$http.post('/authentication/session/new', { request_token: requestToken });
-    if (!res.data.success) {
-      throw new Error('Could not get session id');
-    }
-    return res.data.session_id;
+    this.auth = new AuthenticationService(this.$http);
   }
 }
 
