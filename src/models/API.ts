33df3b06bbbad47ed6b2f9a2321a3e14b670f057
Matteo Gassend <matteo.gassend@epitech.eu>
2022-02-22 11:25:13 +0100

    [add] axios interceptor for logging

--- src/models/API.ts
+++ src/models/API.ts
@@ -1,4 +1,5 @@
 import axios, { AxiosInstance } from 'axios';
+import { $log, Logger } from '@tsed/logger';
 import AuthenticationService from '../services/authentication';
 
 class API {
@@ -6,12 +7,26 @@ class API {
 
   $http: AxiosInstance;
 
+  logger: Logger;
+
   auth: AuthenticationService;
 
-  constructor(apiKey:string) {
+  constructor(apiKey:string, testing = false) {
     this.apiKey = apiKey;
     this.$http = axios.create({ baseURL: 'https://api.themoviedb.org/3', params: { api_key: apiKey } });
-
+    this.logger = $log;
+    this.logger.name = 'TheMovieGetter';
+    this.logger.level = 'debug';
+    if (testing) {
+      this.$http.interceptors.response.use((value) => {
+        this.logger.info(`${value.config.url}`);
+        return value;
+      }, (error) => {
+        this.logger.error(`Request for url ${error.config.url} failed with error ${error.response.status}`);
+        this.logger.error(`query data: ${JSON.stringify(error.config.params)}`);
+        Promise.reject(error);
+      });
+    }
     this.auth = new AuthenticationService(this.$http);
   }
 }
