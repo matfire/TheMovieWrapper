cf6adf9447e9058f3f82807afb2dcf224ff8dba5
Matteo Gassend <matteo.gassend@epitech.eu>
2022-02-23 11:17:12 +0100

    [test] check for model value

--- src/__tests__/movie.ts
+++ src/__tests__/movie.ts
@@ -33,3 +33,14 @@ test('get trending movie total results', async () => {
   // eslint-disable-next-line no-undef
   expect(clientRes.total_results).toBe(res.data.total_results);
 });
+
+// eslint-disable-next-line no-undef
+test('check for movie model values', async () => {
+  const clientRes = await client.movies.getTrending('day');
+  const res = await client.$http.get('/trending/movie/day');
+
+  // eslint-disable-next-line no-undef
+  expect(clientRes.results[0].adult).toBe(res.data.results[0].adult);
+  // eslint-disable-next-line no-undef
+  expect(clientRes.results[0].id).toBe(res.data.results[0].id);
+});
