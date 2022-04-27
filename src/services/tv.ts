59c49ded2f8b62b016dbb66786d914bda9581004
Matteo Gassend <mgassend@gmail.com>
2022-04-27 21:24:36 +0200

    [add] latest tv show

--- src/services/tv.ts
+++ src/services/tv.ts
@@ -1,30 +1,39 @@
 import { GenericListResult, TrendingTimeSpan } from '../types/generic';
 import GenericService from '../types/genericService';
-import { SearchTVInput, TV } from '../types/tv';
+import { SearchTVInput, TV, TVDetails } from '../types/tv';
 
 class TVService extends GenericService {
   async getTrending(time: TrendingTimeSpan): Promise<GenericListResult<TV>> {
-    const { data } = await this.$http.get(`/trending/tv/${time}`);
+    const { data } = await this.$http.get(`/trending/tv/${time}`, {
+      params: { ...this.$http.defaults.params, language: this.language },
+    });
     return data;
   }
 
   async search(search: SearchTVInput): Promise<GenericListResult<TV>> {
     const { data } = await this.$http.get('/search/tv', {
-      params: { ...this.$http.defaults.params, ...search },
+      params: { ...this.$http.defaults.params, ...search, language: this.language },
     });
     return data;
   }
 
   async getTopRated(page ?: number): Promise<GenericListResult<TV>> {
     const { data } = await this.$http.get('/tv/top_rated', {
-      params: { ...this.$http.defaults.params, page },
+      params: { ...this.$http.defaults.params, page, language: this.language },
     });
     return data;
   }
 
   async getPopular(page ?: number): Promise<GenericListResult<TV>> {
     const { data } = await this.$http.get('/tv/popular', {
-      params: { ...this.$http.defaults.params, page },
+      params: { ...this.$http.defaults.params, page, language: this.language },
+    });
+    return data;
+  }
+
+  async getLatest(): Promise<TVDetails> {
+    const { data } = await this.$http.get('/tv/latest', {
+      params: { ...this.$http.defaults.params, language: this.language },
     });
     return data;
   }
--- src/types/tv.ts
+++ src/types/tv.ts
@@ -1,4 +1,6 @@
-import { Country, Language } from './generic';
+import {
+  Country, Genre, Language, ProductionCompany,
+} from './generic';
 
 interface TV {
   first_air_date: Date;
@@ -23,4 +25,41 @@ interface SearchTVInput {
   first_air_date_year ?: number;
 }
 
-export { TV, SearchTVInput };
+interface SeasonOverview {
+  air_date: Date;
+  episode_count: number;
+  id: number;
+  poster_path: string | null;
+  season_number: number;
+}
+
+interface TVDetails {
+  backdrop_path: string | null;
+  created_by: object[];
+  episode_run_time: number[];
+  first_air_date: Date;
+  genres: Genre[];
+  homepage: string | null;
+  id: number;
+  in_prodution: boolean;
+  languages: Language[];
+  last_air_date: Date | null;
+  name: string;
+  networks: object[]; // TODO create network interface
+  number_of_episodes: number;
+  number_of_seasons: number;
+  origin_country: Country[];
+  original_language: Language;
+  original_name: string;
+  overview: string | null;
+  popularity: number;
+  poster_path: string | null;
+  production_companies: ProductionCompany[];
+  seasons: SeasonOverview[];
+  status: string;
+  type: string;
+  vote_average: number;
+  vote_count: number;
+}
+
+export { TV, SearchTVInput, TVDetails };
