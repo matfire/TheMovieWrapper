8cf81925d98b44caa9b61eb6e49835a3f0377601
Matteo Gassend <mgassend@gmail.com>
2022-10-27 19:30:21 +0200

    [add] todo

--- src/services/tv.ts
+++ src/services/tv.ts
@@ -51,7 +51,7 @@ class TVService extends GenericService {
     });
     return data;
   }
-
+  //TODO add append to response
   async getDetails(id : number): Promise<TVDetails> {
     const { data } = await this.$http.get(`/tv/${id}`, {
       params: { ...this.$http.defaults.params, language: this.language },
