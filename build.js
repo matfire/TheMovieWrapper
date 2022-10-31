a7e03552008abd94e6a2da3d68f6394d8e9c84a8
Matteo Gassend <mgassend@gmail.com>
2022-10-31 11:51:37 +0100

    [fix] reimplemented typings

--- build.js
+++ build.js
@@ -1,29 +1,30 @@
 /* eslint-disable import/no-extraneous-dependencies */
-const { build } = require('esbuild');
-const { Generator } = require('npm-dts');
-const { dependencies } = require('./package.json');
+const { build } = require("esbuild");
+const { Generator } = require("npm-dts");
+const { dependencies } = require("./package.json");
 
 new Generator({
-  entry: 'src/index.ts',
-  output: 'dist/index.d.ts',
+  entry: "src/index.ts",
+  output: "dist/index.d.ts",
 }).generate();
 
 const sharedConfig = {
-  entryPoints: ['src/index.ts'],
+  entryPoints: ["src/index.ts"],
   bundle: true,
   minify: true,
+  sourcemap: true,
   external: Object.keys(dependencies),
 };
 
 build({
   ...sharedConfig,
-  platform: 'node', // for CJS
-  outfile: 'dist/index.js',
+  platform: "node", // for CJS
+  outfile: "dist/index.js",
 });
 
 build({
   ...sharedConfig,
-  outfile: 'dist/index.esm.js',
-  platform: 'neutral', // for ESM
-  format: 'esm',
+  outfile: "dist/index.esm.js",
+  platform: "neutral", // for ESM
+  format: "esm",
 });
