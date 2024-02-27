d12344edbda57f8809bf7309d741b4afb85cf18d
Matteo Gassend <matteo.gassend@agiltech.fr>
2024-02-27 09:12:08 +0100

    fix: export all types & version bump

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,7 @@
 import API from './API';
 
 export default API;
-export { Language, Country } from './types/generic';
+export * from './types/generic';
+export * from './types/movie';
+export * from './types/genre';
+export * from './types/account';
